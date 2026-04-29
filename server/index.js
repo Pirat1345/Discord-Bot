aa1960140a3af936737bf1a5bf12c23ab4821f27
Pirat1345 <Piratensohn@gmx.de>
2026-04-29 17:22:30 +0200

    Fix Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.use('/api/soundboard', soundboardRoutes);
 // Serve built frontend in production
 const distPath = path.join(__dirname, '..', 'dist');
 app.use(express.static(distPath));
-app.get('*', (_req, res) => {
+app.get('{*path}', (_req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
